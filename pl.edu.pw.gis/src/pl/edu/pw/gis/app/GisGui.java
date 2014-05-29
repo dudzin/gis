04e8ad75ff02800134a2612be1d3452e79d75a4f
Mariusz <budkagracza@gmail.com>
2014-05-29 22:48:27 +0200

    last changes: added feateruse of chromatic, performance test, display list of graphs

--- pl.edu.pw.gis/src/pl/edu/pw/gis/app/GisGui.java
+++ pl.edu.pw.gis/src/pl/edu/pw/gis/app/GisGui.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
  * Created by Mariusz on 2014-05-24.
  */
 public class GisGui {
-    private JTextPane programGiSLiczbaChromatywnaTextPane;
+    private JTextPane programGiSLiczbaChromatycznaTextPane;
     private JTextField graphField;
     private JTextPane wpiszGrafTextPane;
     private JButton sprawdzJsonButton;
@@ -26,43 +26,73 @@ public class GisGui {
 
     public JTextArea textConsole;
     private JButton jchoosefileButton;
+    private JButton testWydajnosciButton;
+    private JButton wczytaneGrafyButton;
     protected String fileName;
+    protected JSONReader reader;
+    protected LFAlg chromatic;
 
     public GisGui() {
 
         sprawdzJsonButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                checkGraph();
+                calculateChromatic();
             }
         });
 
         jchoosefileButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //Create a file chooser
+
                 final JFileChooser fc = new JFileChooser();
 
-                //In response to a button click:
                 int returnVal = fc.showOpenDialog(GisGui);
 
                 if (returnVal == JFileChooser.APPROVE_OPTION) {
                     File file = fc.getSelectedFile();
-                    //This is where a real application would open the file.
                     fileName = file.getPath();
                     System.out.println("Opening: " + file.getPath() + ".");
-                    wpiszGrafTextPane.setText(fileName);
+                    graphField.setText(fileName);
+                    sprawdzJsonButton.setEnabled(true);
+                    testWydajnosciButton.setEnabled(true);
+                    wczytaneGrafyButton.setEnabled(true);
+                    checkGraph();
                 } else {
                     System.out.println("Open command cancelled by user.");
                 }
             }
         });
+        testWydajnosciButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performanceTest();
+            }
+        });
+        wczytaneGrafyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int index = 0;
+                for(Graph graph : reader.graphs){
+                    System.out.println("graph " + String.valueOf(index) + ":");
+                    for(Node nod : graph.getNodes()){
+                        String summary = String.valueOf(nod.getNumber()) + ":";
+                        for(Node nd : nod.getNeighbours()){
+                            summary += " " + nd.getNumber();
+                        }
+                        System.out.println(summary);
+                    }
+                    System.out.println("\n");
+                    index++;
+                }
+
+            }
+        });
     }
 
-
     protected void checkGraph() {
-        JSONReader reader;
-        LFAlg chromatic = new LFAlg();
+
+        chromatic = new LFAlg();
         ArrayList<Graph> list = new ArrayList<>();
         if (graphField.getText() != "") {
             reader = new JSONReader(fileName);
@@ -70,27 +100,42 @@ public class GisGui {
             reader = new JSONReader();
         }
         reader.readGraphs();
-        for(Graph graph : reader.graphs){
-            System.out.println("graf:");
-            for(Node nod : graph.getNodes()){
-                System.out.println(nod.getNumber() + " " + nod.getNeighbours().toString());
+    }
+
+    protected void calculateChromatic(){
+        int index = 0;
+        for(Graph graph : reader.graphs) {
+            chromatic.setGraph(graph);
+            int q = chromatic.exec();
+            ArrayList<Node> bestColouring = chromatic.getBestGraph().getNodes();
+
+            System.out.println("graph " + String.valueOf(index) + ":");
+            for (int j = 0; j < bestColouring.size(); j++) {
+                System.out.println("Node : " + bestColouring.get(j).getNumber() + "  Colour: " + bestColouring.get(j).getColour());
             }
+            System.out.println("q: " + String.valueOf(q));
+            System.out.println("\n");
+            index++;
+        }
+    }
+
+    protected void performanceTest(){
+        ArrayList<Node> bestColouring;
+        for(int i =5 ;i<reader.graphs.size(); i++){
+            chromatic.setGraph(reader.graphs.get(i));
+            int q = chromatic.exec();
+            bestColouring = chromatic.getBestGraph().getNodes();
+            System.out.println("\nlancuch" + (i-4) + " Graph Statistics:");
+            System.out.println("Q 	          : " + q);
+            System.out.println("Sort 	  Time: " + chromatic.getSortTime());
+            System.out.println("Execution Time: " + chromatic.getExecutionTime());
+            System.out.println("Breaks   Count: " + chromatic.getRejectedCount() + "\n");
 
         }
-        System.out.println("licze");
-        chromatic.setGraph(reader.graphs.get(0));
-        chromatic.exec();
-        ArrayList<Node> bestColouring = chromatic.getBestGraph().getNodes();
-
-        System.out.println("best graph");
-        for(int j =0 ;j< bestColouring.size() ; j++){
-            System.out.println("Node : " + bestColouring.get(j).getNumber()+"  Colour: " + bestColouring.get(j).getColour());
-        }
-
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("GisGui");
+        JFrame frame = new JFrame("Program GiS - Liczba Chromatyczna");
 
         GisGui panel = new pl.edu.pw.gis.app.GisGui();
         frame.setContentPane(panel.GisGui);
